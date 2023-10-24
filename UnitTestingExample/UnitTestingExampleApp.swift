73be6899df47884b1ca541e37ee26f855107d8ac
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-10-24 18:42:49 +0530

    First commit

---
+++ UnitTestingExample/UnitTestingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingExampleApp.swift
+//  UnitTestingExample
+//
+//  Created by MAHESHWARAN on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
